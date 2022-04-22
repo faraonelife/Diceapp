a45a9d6c96264cc7fe3e9ca5d14ec8c0145721c1
Farxod Khaitbaev <72006448+faraonelife@users.noreply.github.com>
2022-04-22 19:10:39 +0500

    Dice App finished

---
+++ app/src/main/java/com/faraonelife/diceapp/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.faraonelife.diceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceOneImg: ImageView
+    lateinit var diceTwoImg: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        diceOneImg=findViewById(R.id.dice_one)
+        diceTwoImg=findViewById(R.id.dice_two)
+
+        val rollButton =findViewById<Button>(R.id.btn_roll_dice)
+        rollButton.setOnClickListener{
+            rollDice()
+        }
+    }
+    fun rollDice(){
+
+        val randomNumber1=(1..6).random()
+        val randomNumber2=(1..6).random()
+
+        val image1 = when(randomNumber1){
+            1 ->R.drawable.dice1
+            2 ->R.drawable.dice2
+            3 ->R.drawable.dice3
+            4 ->R.drawable.dice4
+            5 ->R.drawable.dice5
+            6 ->R.drawable.dice6
+           else -> R.drawable.dice_empty
+       }
+        val image2 = when(randomNumber2){
+            1 ->R.drawable.dice1
+            2 ->R.drawable.dice2
+            3 ->R.drawable.dice3
+            4 ->R.drawable.dice4
+            5 ->R.drawable.dice5
+            6 ->R.drawable.dice6
+            else -> R.drawable.dice_empty
+        }
+        diceOneImg.setImageResource(image1)
+        diceTwoImg.setImageResource(image2)
+    }
+}
